2b935120ae11468872b4d244f8b0c3d5c1c34d7d
sandracubero <sandracuberor@gmail.com>
2019-09-09 20:57:57 +0200

    Fix some mistakes. Created social icons

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
-class Icon extends React.Component {
-  render() {
-    return (
-      <a className={this.props.aClass} href={this.props.aHref} target={this.props.aTarget}>
-        <i className={this.props.fontAwesome + this.props.iconJsClass + "js-icon-preview"}></i>
-      </a>
-    );
-  }
 
-export defualt Icon;
+const Icon = props => {
+
+  return (
+    <a className={props.aClass} href={props.aHref} target={props.aTarget}>
+      <i className={props.fontAwesome + " " + props.iconJsClass + " js-icon-preview"}></i>
+    </a>
+  );
+}
+
+export default Icon;
