01e4ccf2c1e387c0cdc8a42c4a227416f287e0fd
Laura Ruiz <laura.ruizmazorra@gmail.com>
2019-09-10 11:06:32 +0200

    Add Label and Input components to Form

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 // import '../scss/App.scss';
 import Collapsible from './Collapsible'
 import Share from './Share';
+import Form from './Form';
 
 function App() {
   return (
     <div className="App">
-      <Share/>
+      <Form />
+      <Share />
     </div>
   );
 }
---
+++ src/components/Form.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import Collapsible from './Collapsible';
+
+
+{/* 
+
+
+<div class="phone">
+<label class="form__item" for="phone">Teléfono</label>
+<input class="item__input js-input-phone" type="tel" id="phone" name="phone" placeholder="Ej: 555-55-55-55" />
+</div>  */}
+
+
+class Form extends React.Component {
+    render() {
+        const asterisk = <span className="asterisk">*</span>;
+        return (
+            <div className="fillin">
+                <Collapsible />
+                <div className="name" /*Este DIV debería ser otro componente mas?*/>
+                    <Label htmlFor="name" text="Nombre Completo" asterisk={asterisk} />
+                    <Input inputClass="item__input" id="name" type="text" name="name" placeholder="Ej: Sally Jill" required="required" />
+                </div>
+                <div className="job">
+                    <Label htmlFor="job" text="Puesto" asterisk={asterisk} />
+                    <Input inputClass="item__input js-input-job" id="job" type="text" name="name" placeholder="Ej: Front-end unicorn" required="required" />
+                </div>
+                <div className="photo">
+                    <Label htmlFor="photo" text="Imagen de Perfil" asterisk={asterisk} />
+                    <div className="photo__container">
+                        <Input inputClass="js-form__photo form__photo" id="files" type="file" name="photo" required="required" />
+                        <Label className="image__btn" htmlFor="files" text="Añadir imagen" />
+                        <img class="js-preview js-photo preview" />
+                    </div>
+                </div>
+                <div className="email">
+                    <Label htmlFor="email" text="Email" asterisk={asterisk} />
+                    <Input inputClass="item__input js-input-email" id="email" type="email" name="email" placeholder="Ej: sally-hill@gmail.com" required="required" />
+                </div>
+                <div className="phone">
+                    <Label htmlFor="phone" text="Teléfono" />
+                    <Input inputClass="item__input js-input-phone" type="tel" id="phone" name="phone" placeholder="Ej: 555-55-55-55" />
+                </div>
+            </div>)
+
+    }
+}
+
+
+class Label extends React.Component {
+    render() {
+        return (
+            <label className="form__item" htmlFor={this.props.htmlFor}> {this.props.text} {this.props.asterisk}</label>
+        )
+    }
+}
+
+class Input extends React.Component {
+    render() {
+        return (
+            <input className={this.props.inputClass} id={this.props.id} type={this.props.type} name={this.props.name} placeholder={this.props.placeholder} required={this.props.required} />
+        )
+    }
+}
+
+
+
+
+
+
+
+
+
+export default Form;
