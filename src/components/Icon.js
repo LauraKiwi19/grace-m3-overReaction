8cffa73c549d6cb60c6bbc43bd367b4548102183
sandracubero <sandracuberor@gmail.com>
2019-09-17 20:43:16 +0200

    Improve handleChange to inputs so they don't render every time they change

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -2,16 +2,12 @@ import React from "react";
 
 const Icon = props => {
   const { phone, email, likedin, github } = props.iconsList;
-
-  // console.log(props.iconsList);
-  // console.log(props.iconsList);
   let opacity = "0.5";
 
   const iconOpacity = () => {
     for (let icon in props.iconsList) {
       if (props.id === icon.toString()) {
         if (props.iconsList[icon] !== "") {
-          console.log(props.iconsList[icon]);
           opacity = "1";
         } else {
           opacity = "0.5";
--- src/components/Input.js
+++ src/components/Input.js
@@ -1,14 +1,15 @@
 import React from "react";
 
+const handleChange = (props, ev) => {
+  const inputValue = ev.target.value;
+  const inputName = ev.target.name;
+  return props.getInputValues(inputName, inputValue);
+};
+
 const Input = props => {
-  const handleChange = ev => {
-    const inputValue = ev.target.value;
-    const inputName = ev.target.name;
-    return props.getInputValues(inputName, inputValue);
-  };
   return (
     <input
-      onChange={handleChange}
+    onChange={handleChange.bind(null, props)}
       className={props.inputClass}
       value={props.inputValue}
       id={props.id}
