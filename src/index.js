b8a250686aa433f315d8023ce558147ae7067483
Bárbara Chaves <barbarabarcellos90@gmail.com>
2019-09-17 09:55:40 +0200

    Revert "Preview colors"

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './scss/index.scss';
+import './stylesheets/index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
