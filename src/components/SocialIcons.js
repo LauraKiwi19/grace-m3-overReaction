5e4e3c63f8cbc9f5d110d538f5abd1a472033b6e
Ana Amaro <amarovazquezana@gmail.com>
2019-09-20 21:15:34 +0200

    add social icons

--- src/components/SocialIcons.js
+++ src/components/SocialIcons.js
@@ -1,46 +1,43 @@
-import React from "react";
-import Icon from "./Icon";
+import React from 'react';
+import Icon from './Icon';
 
 const SocialIcons = props => {
-  
   return (
-    <div className="card__social">
-      <div className="card__social--div">
+    <div className='card__social'>
+      <div className='card__social--div'>
         <Icon
-          aClass="card__social--button"
-          aHref=""
-          aTarget="_blank"
-          fontAwesome="fas fa-mobile-alt"
-          iconJsClass="js-icon-phone"
+          aClass='card__social--button'
+          fontAwesome='fas fa-mobile-alt'
+          iconJsClass='js-icon-phone'
           iconsList={props.iconsList}
-          id="phone"
+          id='phone'
         />
         <Icon
-          aClass="card__social--button js-icon-link-email"
-          aHref=""
-          aTarget="_blank"
-          fontAwesome="fab fas fa-envelope"
-          iconJsClass="js-icon-email"
+          aClass='card__social--button js-icon-link-email'
+          aHref={'https://' + props.iconsList.email}
+          aTarget='_blank'
+          fontAwesome='fab fas fa-envelope'
+          iconJsClass='js-icon-email'
           iconsList={props.iconsList}
-          id="email"
+          id='email'
         />
         <Icon
-          aClass="card__social--button js-icon-link-linkedin"
-          aHref=""
-          aTarget="_blank"
-          fontAwesome="fab fa-linkedin-in"
-          iconJsClass="js-icon-linkedin"
+          aClass='card__social--button js-icon-link-linkedin'
+          aHref={'https://linkedin.com/in/' + props.iconsList.linkedin}
+          aTarget='_blank'
+          fontAwesome='fab fa-linkedin-in'
+          iconJsClass='js-icon-linkedin'
           iconsList={props.iconsList}
-          id="linkedin"
+          id='linkedin'
         />
         <Icon
-          aClass="card__social--button js-icon-link-github"
-          aHref=""
-          aTarget="_blank"
-          fontAwesome="fab  fa-github-alt"
-          iconJsClass="js-icon-github"
+          aClass='card__social--button js-icon-link-github'
+          aHref={'https://github.com/' + props.iconsList.github}
+          aTarget='_blank'
+          fontAwesome='fab  fa-github-alt'
+          iconJsClass='js-icon-github'
           iconsList={props.iconsList}
-          id="github"
+          id='github'
         />
       </div>
     </div>
