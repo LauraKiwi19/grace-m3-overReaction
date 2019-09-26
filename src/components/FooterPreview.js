ef4654f78657477cc5da83776eefc6ae2e6220d9
Bárbara Chaves <barbarabarcellos90@gmail.com>
2019-09-26 17:41:49 +0200

    Update FooterPreview.js
    fix copyrigth

--- src/components/FooterPreview.js
+++ src/components/FooterPreview.js
@@ -5,7 +5,7 @@ const FooterPreview = () => {
     return (
         <footer className="footer">
             <div className="footer__container">
-                <p className="footer__copy">Awesome profile-cards &copy 2019</p>
+                <p className="footer__copy">Awesome profile-cards {'\u00A9'} 2019</p>
                 <a href="https://adalab.es/" target="_blank" rel="noopener noreferrer" >
                     <img className="logo__adalab" src={adalabLogo} alt="Link a la página de Adalab" />
                 </a>
